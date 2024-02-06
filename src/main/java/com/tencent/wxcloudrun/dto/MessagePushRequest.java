8e279f2f45294ece1c8acf503f986d563809d979
伯鉴 <bo.ybyb@alipay.com>
2024-02-06 16:32:38 +0800

    Fix转发消息取值为空问题V2

--- src/main/java/com/tencent/wxcloudrun/dto/MessagePushRequest.java
+++ src/main/java/com/tencent/wxcloudrun/dto/MessagePushRequest.java
@@ -1,20 +1,27 @@
 package com.tencent.wxcloudrun.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class MessagePushRequest {
 
+  @JsonProperty("ToUserName")
   private String toUserName;
 
+  @JsonProperty("FromUserName")
   private String fromUserName;
 
+  @JsonProperty("CreateTime")
   private String createTime;
 
+  @JsonProperty("MsgType")
   private String msgType;
 
+  @JsonProperty("Content")
   private String content;
 
+  @JsonProperty("MsgId")
   private String msgId;
 
 
