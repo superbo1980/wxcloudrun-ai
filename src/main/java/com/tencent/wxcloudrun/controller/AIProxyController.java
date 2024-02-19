75df445976856ef4b5800ba98ea839abfa9222a2
伯鉴 <bo.ybyb@alipay.com>
2024-02-19 21:01:34 +0800

    接入客服消息API接口支持异步发送消息V1

--- src/main/java/com/tencent/wxcloudrun/controller/AIProxyController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/AIProxyController.java
@@ -7,13 +7,23 @@ import com.alibaba.dashscope.aigc.conversation.ConversationResult;
 import com.alibaba.dashscope.aigc.generation.GenerationOutput;
 import com.alibaba.dashscope.utils.JsonUtils;
 import com.tencent.wxcloudrun.dto.MessagePushRequest;
-import dev.langchain4j.model.openai.OpenAiChatModel;
+import io.reactivex.Flowable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -66,39 +76,107 @@ public class AIProxyController {
 
     JSONObject jsonObject = new JSONObject();
     try{
-      jsonObject.put("ToUserName", request.getFromUserName());
-      jsonObject.put("FromUserName", request.getToUserName());
-      jsonObject.put("MsgType", "text");
-      jsonObject.put("CreateTime", System.currentTimeMillis());
+
+      jsonObject.put("touser", request.getFromUserName());
+      jsonObject.put("msgtype", "text");
+      Map<String,String> textValueMap = new HashMap<>();
+      textValueMap.put("content", "API主动回复消息");
+      jsonObject.put("text", textValueMap);
+      customSend(jsonObject);
+
+
+//      jsonObject.put("CreateTime", System.currentTimeMillis());
+
+//      jsonObject.put("ToUserName", request.getFromUserName());
+//      jsonObject.put("FromUserName", request.getToUserName());
+//      jsonObject.put("MsgType", "text");
+//      jsonObject.put("CreateTime", System.currentTimeMillis());
 
       // 调用chatModel生成聊天内容
 //      String answer = chatModel.generate(request.getContent());
 
-      builder.prompt(request.getContent());
-      ConversationResult result = conversation.call(builder.build());
-      GenerationOutput output = result.getOutput();
-      jsonObject.put("Content", output.getText());
-      return jsonObject.toString();
+//      builder.prompt(request.getContent());
+//      ConversationResult result = conversation.call(builder.build());
+//      GenerationOutput output = result.getOutput();
+//      jsonObject.put("Content", output.getText());
+//      return jsonObject.toString();
 
     }catch (Exception e) {
       logger.error("有异常",e);
-      return "success";
     }
+    return "success";
 
   }
+
+  private  void customSend(JSONObject msgBody) throws Exception{
+
+    HttpURLConnection con = null;
+    BufferedReader buffer = null;
+    StringBuffer resultBuffer = null;
+    PrintWriter out = null;
+    URL url = null;
+
+      url = new URL("http://api.weixin.qq.com/cgi-bin/message/custom/send");
+      con = (HttpURLConnection) url.openConnection();
+      con.setRequestMethod("POST");
+      con.setRequestProperty("Content-Type", "application/json");
+      con.setRequestProperty("Accept", "*/*");
+      con.setUseCaches(false);
+      con.setDoInput(true);
+      con.setDoOutput(true);
+      out = new PrintWriter(con.getOutputStream());
+      out.print(msgBody.toString());
+      out.flush();
+
+      int responseCode = con.getResponseCode();
+      if (responseCode == HttpURLConnection.HTTP_OK) {
+        //得到响应流
+        InputStream inputStream = con.getInputStream();
+        //将响应流转换成字符串
+        resultBuffer = new StringBuffer();
+        String line;
+        buffer = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+        while ((line = buffer.readLine()) != null) {
+          resultBuffer.append(line);
+        }
+        System.out.println("result:" + resultBuffer.toString());
+      }
+  }
+
 //
-//  public static void main(String[] args) {
-//
-//    try {
+  public static void main(String[] args) {
+
+    try {
+
+      JSONObject jsonObject = new JSONObject();
+      jsonObject.put("touser", "openid-001");
+      jsonObject.put("msgtype", "text");
+      Map<String,String> textValueMap = new HashMap<>();
+      textValueMap.put("content", "API主动回复消息");
+      jsonObject.put("text", textValueMap);
+      System.out.println(jsonObject.toString());
+
 //      AIProxyController aiProxyController = new AIProxyController();
 //      aiProxyController.builder.prompt("你是一名文学专家，请帮我梳理一下作品《红楼梦》中的人物关系");
+//
+//      Flowable<ConversationResult> result = aiProxyController.conversation.streamCall(aiProxyController.builder.build());
+//      result.blockingForEach(msg->{
+//
+//        PrintWriter out = new PrintWriter(System.out, true);
+//        out.println(msg.getOutput().getText());
+//        System.out.print(msg.getOutput().getText());
+//     });
+
+//      System.exit(0);
+
+
 //      ConversationResult result = aiProxyController.conversation.call(aiProxyController.builder.build());
 //      GenerationOutput output = result.getOutput();
 //      System.out.println(output.getText());
-//    }catch (Exception e) {
-//      e.printStackTrace();
-//    }
-//  }
+    }catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
   
 }
